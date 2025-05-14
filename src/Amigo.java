a305b3873a6fbe06666b5ee77bcadd31cc5eb7c1
João Geraldo Bussolo Bevilacqua <joaogeraldobussolo@gmail.com>
2025-05-14 09:38:21 -0300

    14/05

--- src/Amigo.java
+++ src/Amigo.java
@@ -1,4 +1,27 @@
 
 public class Amigo {
-
+	private String nome, sexo, mensagem;
+	private int idade;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getSexo() {
+		return sexo;
+	}
+	
+	public String getMensagem() {
+		return mensagem;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	
+	public void SetNome(String nome) {
+		this.nome = nome;
+	}
+	
 }
