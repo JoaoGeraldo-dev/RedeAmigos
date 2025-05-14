b0009fbc615218dbcb98caba1054a7745ec99ad3
João Geraldo Bussolo Bevilacqua <joaogeraldobussolo@gmail.com>
2025-05-14 10:32:44 -0300

    14/05

--- src/Amigo.java
+++ src/Amigo.java
@@ -24,4 +24,28 @@ public class Amigo {
 		this.nome = nome;
 	}
 	
+	public void SetSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	
+	public void SetMensagem(String mensagem) {
+		//o tamanho da mensagem vai ser no maximo 144
+
+		if(mensagem.length() <= 144) {
+			this.mensagem = mensagem;
+		}else {
+			//trunca se for maior
+			this.mensagem = mensagem.substring(0, 144);
+		}	
+	}
+	
+	public void Setidade(int idade) {
+		this.idade = idade;
+	}
+	
+	public String toString() {
+	      return "[Nome: "+nome+"] [Sexo: "+sexo+"] [Idade: "+idade+ 
+	    	         "]\n[Mensagem: "+mensagem+"]"; 
+
+	}
 }
---
+++ src/Rede.java
@@ -0,0 +1,99 @@
+import java.util.ArrayList;
+public class Rede {
+	private ArrayList<Amigo> amigos; 
+	
+	public Rede() {
+		amigos = new ArrayList<Amigo>();
+	}
+	
+	
+	public void addAmigo(Amigo amigo) {
+		amigos.add(amigo);
+	}
+	
+	public boolean blockAmigo(String nome) {
+		int posicao = buscar(nome);
+		
+		if(posicao > 0) {
+			amigos.remove(posicao);
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public int procurarAmigo(String nome) {
+		return buscar(nome);
+	}
+	
+	public boolean enviarMensagem(String nome, String mensagem) {
+		
+		int posicao = buscar(nome);
+		
+		if(posicao >= 1) {
+			Amigo amigo = amigos.get(posicao);
+			amigo.SetMensagem(mensagem);
+			return true;
+		}else {
+			return false;
+		}
+	}
+		
+		public Amigo[] procurarVelhos() {	
+		if(amigos.size() == 0) {
+				return new Amigo[0];
+		}
+		int maior = amigos.get(0).getIdade();
+		//encontrar a maior idade
+			for(Amigo amigo:amigos) {
+				if(amigo.getIdade() > maior) {
+					maior = amigo.getIdade();
+				}
+			}
+			//contar quantos tem a maior idade 
+		int qtde = 0;
+		for (Amigo amigo:amigos) {
+			if(amigo.getIdade() == maior) {
+				qtde++;
+			}
+		}
+		//criar o vetor de amigos
+		Amigo[] velhos = new Amigo[qtde];
+		//pupular o vetor de mais velhos
+		int k = 0;
+		for(int i = 0; i < amigos.size(); i++) {
+			Amigo amigo = amigos.get(i);
+			if(amigo.getIdade() == maior) {
+				velhos[k++] = amigo;
+			}
+		}
+		return velhos;
+		
+	}
+	
+	private int buscar(String nome){ 
+		for(int i = 0; i < amigos.size(); i++){ 
+		Amigo amigo = amigos.get(i); 
+		String nomeAmigo = amigo.getNome(); 
+		if(nome.equals(nomeAmigo)){ 
+		return i; 
+		} 
+		} 
+		return -1;//nao achou 
+		} 
+	
+	public void addAmigo(String nome, String sexo, int idade) {
+		Amigo amigo = new Amigo();
+		
+		amigo.SetNome(nome);
+		amigo.SetSexo(sexo);
+		amigo.Setidade(idade);
+		amigos.add(amigo);
+	}
+	
+	public void listarAmigos() {
+		for(Amigo amigo:amigos) {
+			System.out.println(amigo);
+		}
+	}
+}
